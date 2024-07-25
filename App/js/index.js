cd6ccd12caaeca05731da92877d3069e380215c8
aligithub12-zzz <ali1049944@gmail.com>
2024-07-25 19:04:03 +0400

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -23,21 +23,28 @@ xui.Class('App', 'xui.Module',{
             append(
                 xui.create("xui.UI.Label")
                 .setHost(host,"xui_ui_label7")
-                .setLeft("25.904761904761905em")
-                .setTop("19.80952380952381em")
-                .setWidth("10.81904761904762em")
-                .setHeight("2.742857142857143em")
+                .setLeft("44.19047619047619em")
+                .setTop("40.38095238095238em")
+                .setWidth("53.48571428571429em")
+                .setHeight("5.79047619047619em")
                 .setCaption("WINDOWS IS COMING")
+                .setHAlign("center")
+                .setFontColor("#FF0000")
+                .setFontSize("5em")
+                .setFontFamily("arial")
             );
             
             append(
                 xui.create("xui.UI.Button")
                 .setHost(host,"xui_ui_button7")
-                .setLeft("23.61904761904762em")
-                .setTop("25.904761904761905em")
+                .setLeft("63.23809523809524em")
+                .setTop("46.476190476190474em")
                 .setWidth("15.161904761904761em")
                 .setHeight("5.104761904761904em")
                 .setCaption("ok I understand.")
+                .setFontColor("#FF0000")
+                .setFontSize("1.5em")
+                .setFontFamily("arial")
             );
             
             append(
@@ -56,8 +63,8 @@ xui.Class('App', 'xui.Module',{
                 xui.create("xui.UI.ProgressBar")
                 .setHost(host,"xui_ui_progressbar2")
                 .setLeft("0em")
-                .setTop("43.42857142857143em")
-                .setWidth("60.8em")
+                .setTop("68.57142857142857em")
+                .setWidth("142.32380952380953em")
                 .setHeight("2.2857142857142856em")
                 .setFillBG("#7FFF00")
                 .setValue(75)
